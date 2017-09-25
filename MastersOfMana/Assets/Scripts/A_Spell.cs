2ae47999ee99ff2d01534a81ce9e018ec22eb34c
Lukas Steinmetz <dr.spoik@gmail.com>
2017-09-25 14:25:37 +0200

    add basic spell architecture

---
+++ MastersOfMana/Assets/Scripts/A_Spell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class A_Spell : ScriptableObject 
+{
+	public UnityEngine.UI.Image icon;
+	public GameObject[] spawningObjects;
+
+	public abstract void Cast();
+}
+
---
+++ MastersOfMana/Assets/Scripts/Effect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Effect", menuName = "Spells/Effects", order = 1)]
+public class Effect : A_Spell 
+{
+	#region implemented abstract members of A_Spell
+
+	public override void Cast ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	#endregion
+}

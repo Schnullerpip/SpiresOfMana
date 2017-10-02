314218842565d8ae8e2f68524c51704bf2ea0ed6
Julian <Schnullerpip@gmail.com>
2017-10-02 10:13:45 +0200

    Feature: improved PoolRegistry

--- MastersOfMana/Assets/Scripts/Spells/SpellBehaviour/FireballBehaviour.cs
+++ MastersOfMana/Assets/Scripts/Spells/SpellBehaviour/FireballBehaviour.cs
@@ -6,7 +6,7 @@ public class FireballBehaviour : A_SpellBehaviour {
 
     public override void Execute(PlayerScript caster)
     {
-        GameObject fireball = PoolRegistryScript.FireballPool.Get();
+        GameObject fireball = PoolRegistry.FireballPool.Get();
         fireball.transform.position = caster.transform.position + caster.transform.forward * 0.5f;
     }
 }

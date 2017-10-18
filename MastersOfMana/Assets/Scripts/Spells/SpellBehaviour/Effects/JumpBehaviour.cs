b47e62333f85f9a7f8ed81745b9655933d98de8f
Lukas Steinmetz <dr.spoik@gmail.com>
2017-10-18 19:53:16 +0200

    Feature: add jump spell

---
+++ MastersOfMana/Assets/Scripts/Spells/SpellBehaviour/Effects/JumpBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBehaviour : A_EffectBehaviour 
+{
+	public float jumpForce = 15.0f;
+	public float pullInRadius = 4.0f;
+	public float pullInForce = 600;
+
+	public override void Execute(PlayerScript caster)
+	{
+		caster.Jump(jumpForce);
+		Collider[] cols = Physics.OverlapSphere(caster.transform.position, pullInRadius);
+
+		foreach(Collider c in cols)
+		{
+			if(c.attachedRigidbody != null && c.attachedRigidbody.gameObject != caster.gameObject)
+			{
+				c.attachedRigidbody.AddExplosionForce(-pullInForce, caster.transform.position, pullInRadius);
+			}
+		}
+	}
+}

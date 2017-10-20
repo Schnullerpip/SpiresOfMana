9498083fb8d91aead401da2d905ab3f5a08296a3
Arne <arne.knobloch@gmx.de>
2017-10-20 14:57:29 +0200

    BUG: LavaPool only damage on the server BUG: Some Cleanup in Setupmanagers

--- MastersOfMana/Assets/LavaFloor.cs
+++ MastersOfMana/Assets/LavaFloor.cs
@@ -22,10 +22,13 @@ public class LavaFloor : NetworkBehaviour
 
     public void OnTriggerStay(Collider other)
     {
-        PlayerScript player = other.GetComponentInParent<PlayerScript>();
-        if (player && isServer)
+        if (isServer)
         {
-            player.healthScript.TakeDamage(damagePerSecond * Time.deltaTime);
+            PlayerScript player = other.GetComponentInParent<PlayerScript>();
+            if (player && isServer)
+            {
+                player.healthScript.TakeDamage(damagePerSecond * Time.deltaTime);
+            }
         }
     }
 

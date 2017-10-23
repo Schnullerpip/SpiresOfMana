ad7c9d572afb73a5b5f793cfe7aed8b1e624ec53
Lukas Steinmetz <dr.spoik@gmail.com>
2017-10-23 12:47:38 +0200

    Bug: Enable controls on client on startup

--- MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/LobbyPlayer.cs
+++ MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/LobbyPlayer.cs
@@ -299,6 +299,9 @@ namespace Prototype.NetworkLobby
         {
             LobbyManager.s_Singleton.mainMenu.countdownPanel.UIText.text = "Match Starting in " + countdown;
             LobbyManager.s_Singleton.mainMenu.countdownPanel.gameObject.SetActive(countdown != 0);
+
+			LobbyManager.s_Singleton.mainMenu.GetRewiredPlayer().controllers.maps.SetMapsEnabled(false,"UI");
+			LobbyManager.s_Singleton.mainMenu.GetRewiredPlayer().controllers.maps.SetMapsEnabled(true,"Default");
         }
 
         [ClientRpc]

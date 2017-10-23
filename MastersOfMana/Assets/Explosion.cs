b20f979633eda43ea9309052a3e141a7071df001
Julian <Schnullerpip@gmail.com>
2017-10-23 16:14:00 +0200

    FEATURE: Dash improved and made an Explosionprefab - also started to implement fistoffury with said exposionprefab

--- MastersOfMana/Assets/Explosion.cs
+++ MastersOfMana/Assets/Explosion.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Explosion : MonoBehaviour {
+public class Explosion : NetworkBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AnimationCurve aCurve;
+    private float mCount = 0;
+    public float amplitude = 1;
+    public Transform explosionMesh;
+    
+    [SerializeField]
+    private float mLifeTime;
+
+    private void EvaluateExplosion()
+    {
+        explosionMesh.localScale = Vector3.one*aCurve.Evaluate(mCount/mLifeTime)*amplitude;
+    }
+
+    public void OnEnable()
+    {
+        mCount = 0;
+        EvaluateExplosion();
+    }
+
+    public void Update()
+    {
+        EvaluateExplosion();
+
+        mCount += Time.deltaTime;
+
+        if (isServer && (mCount >= mLifeTime))
+        {
+            mCount = 0;
+            gameObject.SetActive(false);
+            NetworkServer.UnSpawn(gameObject);
+        }
+    }
 }
--- MastersOfMana/Assets/FistOfFuryBehaviour.cs
+++ MastersOfMana/Assets/FistOfFuryBehaviour.cs
@@ -7,12 +7,18 @@ public class FistOfFuryBehaviour : A_EffectBehaviour
 {
 
     [SerializeField] private GameObject mExplosionPrefab;
+    [SerializeField] private float explosionAmplitude;
 
     public override void Execute(PlayerScript caster)
     {
         GameObject go = PoolRegistry.ExplosionPool.Get();
-        go.transform.position = caster.transform.position;
+        go.transform.position = caster.transform.position/* + caster.transform.forward * 5*/;
         go.SetActive(true);
+        Explosion ex = go.GetComponent<Explosion>();
+        if (ex)
+        {
+            ex.amplitude = explosionAmplitude;
+        }
         NetworkServer.Spawn(go);
     }
 }

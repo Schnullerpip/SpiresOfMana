b7de28ca11f79e7120d8898619c1e2bc597e1003
Arne <arne.knobloch@gmx.de>
2017-10-27 16:25:08 +0200

    BUG: Poolregistry now networkbehaviour

--- MastersOfMana/Assets/Scripts/PoolRegistry.cs
+++ MastersOfMana/Assets/Scripts/PoolRegistry.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PoolRegistry : MonoBehaviour {
+public class PoolRegistry : NetworkBehaviour {
 
     public static Pool
 		EarthwallPool,

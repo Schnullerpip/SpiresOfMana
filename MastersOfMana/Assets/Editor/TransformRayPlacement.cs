de4f42304bddb2e1c58066e051091b45babc8eb3
Lukas Steinmetz <dr.spoik@gmail.com>
2017-11-06 15:45:57 +0100

    added a editor tool to place transforms onto meshes with a raycast

---
+++ MastersOfMana/Assets/Editor/TransformRayPlacement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TransformRayPlacement : EditorWindow 
+{
+	bool isActive = false;
+	string stringActivate = "Activate";
+	string stringDeactivate = "Deactivate";
+
+	static Transform[] selectedTransforms;
+
+	[MenuItem ("Tools/Transform Ray Placement")]
+	static void Init () 
+	{
+		TransformRayPlacement window = (TransformRayPlacement) EditorWindow.GetWindow (typeof (TransformRayPlacement));
+		window.Show();
+	}
+
+	void OnSelectionChange()
+	{
+		Repaint();
+	}
+
+	void OnGUI () 
+	{
+		selectedTransforms = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+		GUILayout.Label (selectedTransforms.Length+" Objects selected");
+
+		GUI.enabled = selectedTransforms.Length != 0 || isActive;
+
+		if(GUILayout.Button(isActive ? stringDeactivate : stringActivate))
+		{
+			isActive = !isActive;
+
+			if(isActive)
+			{
+				SceneView.onSceneGUIDelegate += SceneGUI;
+			}
+			else
+			{
+				SceneView.onSceneGUIDelegate -= SceneGUI;
+			}
+		}
+	}
+		
+	static void SceneGUI(SceneView sceneView)
+	{	
+		Event e = Event.current;
+
+		int controlId = GUIUtility.GetControlID(FocusType.Passive);
+
+		if(e.type == EventType.MouseDown && e.button == 0 && e.modifiers == EventModifiers.Shift)
+		{
+			GUIUtility.hotControl = controlId;
+			e.Use();  //Eat the event so it doesn't propagate through the editor.
+
+			Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit))
+			{
+				Undo.RecordObjects (selectedTransforms, "Move Objects");
+
+				foreach (Transform t in selectedTransforms) {
+					t.position = hit.point;
+				}
+			}
+		}
+	}
+}

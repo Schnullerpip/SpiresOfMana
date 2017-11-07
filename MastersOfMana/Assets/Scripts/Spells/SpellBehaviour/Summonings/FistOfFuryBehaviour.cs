0d5c3ce05da12ad2cd1dacb047b53f6de7dc7e52
Julian <Schnullerpip@gmail.com>
2017-11-07 17:12:53 +0100

    BUG:FIX: FistOfFury moves opponent

--- MastersOfMana/Assets/Scripts/Spells/SpellBehaviour/Summonings/FistOfFuryBehaviour.cs
+++ MastersOfMana/Assets/Scripts/Spells/SpellBehaviour/Summonings/FistOfFuryBehaviour.cs
@@ -18,10 +18,22 @@ public class FistOfFuryBehaviour : A_SummoningBehaviour
     [SerializeField] private float mMaximumDamage;
     [SerializeField] [Range(0.0f, 1.0f)] private float mVelocityScaledDamageFactor;
 
+    private List<GameObject> mAlreadyHit;
+    private float mHighestMeasuredVelocity = 0;
+
     public override void Awake()
     {
         base.Awake();
         mExplosion.amplitude = explosionAmplitude;
+    }
+
+
+    void Update()
+    {
+        if (caster.movement.GetVelocity().y < mHighestMeasuredVelocity)
+        {
+            mHighestMeasuredVelocity = caster.movement.GetVelocity().y;
+        }
     }
 
     public override void Execute(PlayerScript caster)
@@ -32,6 +44,7 @@ public class FistOfFuryBehaviour : A_SummoningBehaviour
         fof.caster = caster;
         fof.transform.position = caster.transform.position;
         fof.transform.parent = caster.transform;
+        fof.mAlreadyHit = new List<GameObject>();
         //spawn it on all clients
         NetworkServer.Spawn(fof.gameObject);
 
@@ -68,27 +81,31 @@ public class FistOfFuryBehaviour : A_SummoningBehaviour
         for (int i = 0; i < colliders.Length; ++i)
         {
             Rigidbody rigid = colliders[i].attachedRigidbody;
-            if (rigid)
+            if (rigid && !mAlreadyHit.Contains(rigid.gameObject))
             {
-
+                mAlreadyHit.Add(rigid.gameObject);
                 PlayerScript ps = colliders[i].attachedRigidbody.GetComponent<PlayerScript>();
+                Vector3 direction = rigid.position - caster.movement.mRigidbody.position;
                 if (ps)
                 {
                     if (ps != caster)
                     {
-                        ps.movement.RpcAddExplosionForce(mExplosionForce, caster.transform.position, explosionAmplitude);
+                        ps.movement.RpcAddForce(mExplosionForce*direction, ForceMode.VelocityChange);
                     }
                 }
                 else
                 {
-                    rigid.AddExplosionForce(mExplosionForce, caster.transform.position, explosionAmplitude);
+                    rigid.AddForce(mExplosionForce*direction, ForceMode.VelocityChange);
                 }
 
                 HealthScript hs = rigid.GetComponentInParent<HealthScript>();
                 if (hs && hs != caster.healthScript)
                 {
-                    float damage = mMinimumDamage + caster.movement.GetVelocity().magnitude*mVelocityScaledDamageFactor;
+                    if (mHighestMeasuredVelocity < 0) mHighestMeasuredVelocity *= -1;
+                    float damage = mMinimumDamage + mHighestMeasuredVelocity*mVelocityScaledDamageFactor;
                     damage = Mathf.Clamp(damage, mMinimumDamage, mMaximumDamage);
+                    Debug.Log("damage by fist: " + damage);
+                    Debug.Log("highest vel: " + mHighestMeasuredVelocity);
                     hs.TakeDamage(damage);
                 }
             }

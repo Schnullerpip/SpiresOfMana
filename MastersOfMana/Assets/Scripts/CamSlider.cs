be93fd3e9835f26620f5d740da2d65538750e0bb
Lukas Steinmetz <dr.spoik@gmail.com>
2017-11-11 14:04:02 +0100

    Fix: Camera Slider Bug

--- MastersOfMana/Assets/Scripts/CamSlider.cs
+++ MastersOfMana/Assets/Scripts/CamSlider.cs
@@ -6,33 +6,43 @@ public class CamSlider : MonoBehaviour {
 
 	public Transform cam;
 
-	[Tooltip("How fast should the camera adjust? (linear)")]
-	public float speed = 1;
+	[Tooltip("How fast should the camera adjust if something is in the way? (linear)")]
+	public float inSpeed = 10;
+	[Tooltip("How fast should the camera adjust back to its correct position? (linear)")]
+	public float outSpeed = 5;
 	[Tooltip("How close can the camera get to any wall?")]
 	public float wallDistance = 0.1f;
 
 	private Vector3 mLocalEndPosition;
 	private float mMaxDistance;
+	private RaycastHit mHit;
+	private PlayerScript mPlayer;
 
 	void OnEnable () {
 		mLocalEndPosition = cam.localPosition;
 		mMaxDistance = (transform.localPosition - mLocalEndPosition).magnitude;
 	}
 
-	private RaycastHit hit;
+	public void SetPlayer(PlayerScript player)
+	{
+		mPlayer = player;
+	}
 
 	void LateUpdate()
 	{
-//		if(Physics.Linecast(transform.position, transform.TransformPoint(localEndPosition), out hit))
-		if(Physics.SphereCast(transform.position, wallDistance, transform.TransformPoint(mLocalEndPosition) - transform.position, out hit, mMaxDistance))
+		mPlayer.SetColliderIgnoreRaycast(true);
+
+		if(Physics.SphereCast(transform.position, wallDistance, transform.TransformPoint(mLocalEndPosition) - transform.position, out mHit	, mMaxDistance))
 		{
-			cam.position = Vector3.MoveTowards(cam.position,hit.point + hit.normal * wallDistance, speed);
-//			Debug.DrawLine(transform.position, hit.point, Color.yellow);
+			cam.position = Vector3.MoveTowards(cam.position,mHit.point + mHit.normal * wallDistance, inSpeed * Time.deltaTime);
+			Debug.DrawLine(transform.position, mHit.point, Color.yellow);
 		}
 		else
 		{
-			cam.localPosition = Vector3.MoveTowards(cam.localPosition, mLocalEndPosition, speed * Time.deltaTime);
+			cam.localPosition = Vector3.MoveTowards(cam.localPosition, mLocalEndPosition, outSpeed * Time.deltaTime);
 		}
+
+		mPlayer.SetColliderIgnoreRaycast(false);
 	}
 
 	void OnValidate()

cf9c2eb10228652ad50809abe9b95e0f6f41a34c
if13b023 <if13b023@technikum-wien.at>
2017-12-07 22:18:32 +0100

    Feature: Earthwall reworked, does not disappear and unlimited numbers are possible

--- MastersOfMana/Assets/Scripts/Spells/SpellBehaviour/Summonings/EarthwallBehaviour.cs
+++ MastersOfMana/Assets/Scripts/Spells/SpellBehaviour/Summonings/EarthwallBehaviour.cs
@@ -8,19 +8,31 @@ using UnityEngine.Networking;
 /// </summary>
 public class EarthwallBehaviour : A_SummoningBehaviour {
 
+    public float distance = 2;
+
+    private int mWallCount = 0;
+
     //TODO exchange magic numbers with actual data
     public override void Execute(PlayerScript caster)
     {
-        RaycastHit hit;
-        if (Physics.Raycast(caster.transform.position + 0.8f * (caster.transform.forward + caster.transform.up), caster.transform.up * -1, out hit, 50))
-        {
-            //GameObject wall = PoolRegistry.EarthwallPool.Get(Pool.Activation.ReturnActivated);
-            GameObject wall = PoolRegistry.Instantiate(this.gameObject);
-            wall.SetActive(true);
-            wall.transform.position = hit.point;
-            wall.transform.rotation = caster.transform.rotation;
-            NetworkServer.Spawn(wall);
-        }
+        Vector3 aimDirection = GetAim(caster);
+
+        GameObject wall = PoolRegistry.Instantiate(this.gameObject);
+        wall.transform.rotation = Quaternion.LookRotation(aimDirection);
+        wall.transform.position = caster.movement.mRigidbody.worldCenterOfMass + wall.transform.forward * distance;
+        wall.SetActive(true);
+        NetworkServer.Spawn(wall);
+
+        //RaycastHit hit;
+        //if (Physics.Raycast(caster.transform.position + 0.8f * (caster.transform.forward + caster.transform.up), caster.transform.up * -1, out hit, 50))
+        //{
+        //    //GameObject wall = PoolRegistry.EarthwallPool.Get(Pool.Activation.ReturnActivated);
+        //    GameObject wall = PoolRegistry.Instantiate(this.gameObject);
+        //    wall.SetActive(true);
+        //    wall.transform.position = hit.point;
+        //    wall.transform.rotation = caster.transform.rotation;
+        //    NetworkServer.Spawn(wall);
+        //}
     }
 
     protected override void ExecuteCollision_Host(Collision collision)

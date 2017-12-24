8a971a314dd0a880044e122690be868c90d4946d
Lukas Steinmetz <dr.spoik@gmail.com>
2017-12-24 02:32:05 +0100

    added a fancy "pop" animation to the spell buttons

---
+++ MastersOfMana/Assets/UISelectAnimation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISelectAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    public float scaledSize = 1.3f;
+    public AnimationCurve growCurve;
+    public AnimationCurve shrinkCurve;
+    public float speed = 1;
+
+    private Vector3 mInitScale;
+
+    private void Awake()
+    {
+        mInitScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Grow();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Shrink();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Grow();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        Shrink();
+    }
+
+    private void Grow()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleTo(Vector3.one * scaledSize, growCurve));
+    }
+
+    private void Shrink()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleTo(mInitScale, shrinkCurve));
+    }
+
+    private IEnumerator ScaleTo(Vector3 desiredScale, AnimationCurve curve)
+    {
+        float timer = 0;
+
+        Vector3 size = transform.localScale;
+
+        while(timer < 1)
+        {
+            transform.localScale = Vector3.LerpUnclamped(size, desiredScale, curve.Evaluate(timer));
+            timer += Time.deltaTime * speed;
+            yield return null;
+        }
+    }
+}

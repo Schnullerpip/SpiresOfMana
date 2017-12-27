39b28024eb955a6c703bf4ae5637a084f5698d7b
Lukas Steinmetz <dr.spoik@gmail.com>
2017-12-27 01:01:15 +0100

    swapped player hurt sounds, added lava sound and animated lava uvs

---
+++ MastersOfMana/Assets/Scripts/AnimateUVOffset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateUVOffset : MonoBehaviour 
+{
+    public Material material;
+    public string propertyName = "_MainTex";
+
+	public NormalizedAnimationCurve xCurve;
+    public NormalizedAnimationCurve yCurve;
+
+    private Vector2 mUV = new Vector2(0,0);
+
+    private int mPropertyId;
+
+    private void Awake()
+    {
+        mPropertyId = Shader.PropertyToID(propertyName);
+    }
+
+    void Update () 
+    {
+        mUV.x = xCurve.Evaluate(Time.time);
+        mUV.y = yCurve.Evaluate(Time.time);
+
+        material.SetTextureOffset(mPropertyId, mUV);
+	}
+}
---
+++ MastersOfMana/Assets/Scripts/LavaSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaSound : MonoBehaviour {
+
+	public Transform lava;
+
+    public Transform ears;
+
+	void LateUpdate () 
+    {
+	    if(!ears || !ears.gameObject.activeInHierarchy)
+        {
+            AudioListener listener = FindObjectOfType<AudioListener>();
+            if(listener)
+            {
+                ears = listener.transform;
+            }
+        }
+
+        Vector3 pos = ears.position;
+        pos.y = lava.position.y;
+        transform.position = pos;
+	}
+}

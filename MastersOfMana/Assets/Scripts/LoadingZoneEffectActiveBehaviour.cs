dbb726be8c39c314b1cfefc1117e0c058f75f457
if13b023 <if13b023@technikum-wien.at>
2018-01-02 18:26:35 +0100

    Feature: LoadingZones revamped again (is now abstract)

---
+++ MastersOfMana/Assets/Scripts/LoadingZoneEffectActiveBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadingZoneEffectActiveBehaviour : MonoBehaviour {
+
+    public enum DestructionMode { DESTRUCT, DEACTIVATE };
+
+    public float secondsToDelayDestruction = 1;
+    public ParticleSystem[] listOfParticleSystems;
+
+    private GameObject objectToTrack;
+
+    public void SetObjectToTrack(GameObject go)
+    {
+        objectToTrack = go;
+
+        foreach(var ps in listOfParticleSystems)
+        {
+            ps.GetComponent<RFX4_ParticleTrail>().Target = objectToTrack;
+        }
+    }
+
+    public GameObject getTrackedObject()
+    {
+        return objectToTrack;
+    }
+
+    public void Selfdestruct(DestructionMode dm)
+    {
+        Debug.Log(gameObject.name + " destruction called!");
+        foreach(var ps in listOfParticleSystems)
+        {
+            var em = ps.emission;//emission is read-only
+            em.enabled = false;//no reassigning required, because reference
+        }
+
+        StartCoroutine(DestroyAfterDelay(dm));
+    }
+
+    private IEnumerator DestroyAfterDelay(DestructionMode dm)
+    {
+        yield return new WaitForSeconds(secondsToDelayDestruction);
+
+        if (dm == DestructionMode.DESTRUCT)
+        {
+            Destroy(gameObject);
+        }
+        else if (dm == DestructionMode.DEACTIVATE)
+        {
+            Debug.Log(gameObject.name + " deactivated!");
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Unknown DestructionMode entered!");
+        }
+    }
+}

cc2607018ee0f1b5c643db64952a49d9543b7944
Juli Desktop <schnullerpip@gmail.com>
2018-01-05 13:22:46 +0100

    FEATURE: improved dash

---
+++ MastersOfMana/Assets/DashEffectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class DashEffectScript : NetworkBehaviour {
+
+    [SyncVar]
+    public Vector3 from;
+    [SyncVar]
+    public Vector3 to;
+
+    public float Lifetime;
+
+    public float Speed;
+
+
+    private void FixedUpdate()
+    {
+        //continuously move the effect so that the particle trail will emit trailparticles
+        transform.position = Vector3.MoveTowards(transform.position, to, Speed*Time.deltaTime);
+    }
+}

f8dce9b57161bfc3a32e941ee86323ae63fcf10a
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-05 14:07:15 +0100

    WIP different spell animations

--- MastersOfMana/Assets/Scripts/States/CastStateSystem/CastStateResolving.cs
+++ MastersOfMana/Assets/Scripts/States/CastStateSystem/CastStateResolving.cs
@@ -8,9 +8,8 @@ public class CastStateResolving : A_CastState {
 
     public override void Init()
     {
-		player.GetPlayerAnimation().Cast();
-
         //set the cooldown of the current spellslot
-        player.GetPlayerSpells().GetCurrentspell().cooldown = player.GetPlayerSpells().GetCurrentspell().spell.coolDownInSeconds;
+		player.GetPlayerSpells().GetCurrentspell().ResetCooldown();
+        player.GetPlayerAnimation().Cast(player.GetPlayerSpells().GetCurrentspell().spell.castAnimationID);
     }
 }

19cdeaefed4139a7efa8d407e8d3343d1a2a071b
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-06 04:26:09 +0100

    a bit of optimising the thunderstorm

--- MastersOfMana/Assets/Scripts/LightningStrike.cs
+++ MastersOfMana/Assets/Scripts/LightningStrike.cs
@@ -85,6 +85,10 @@ public class LightningStrike : NetworkBehaviour
     void OnEnable()
 	{
         vel = Vector3.zero;
+
+        //to counteract the fact that all strikes have the same material and look the same at the same time
+		line.transform.SetPosition(y: Random.Range(0.0f,-5.0f));
+
         line.SetActive(false);
         blobShadow.orthographicSize = 0;
         blobShadow.gameObject.SetActive(true);
@@ -206,10 +210,16 @@ public class LightningStrike : NetworkBehaviour
             }
         }
 
-        hitAmount = Physics.RaycastNonAlloc(transform.position + Vector3.up * 40, Vector3.down, hits, 40);
+        hitAmount = Physics.RaycastNonAlloc(transform.position + Vector3.up * 40, Vector3.down, hits, 40, Physics.DefaultRaycastLayers);
 
         for (int i = 0; i < hitAmount; ++i)
         {
+            if(hits[i].point.y <= 0.1f)
+            {
+                //skip everything beneath the lava
+                continue;
+            }
+
             GameObject decal = PoolRegistry.GetInstance(contactPrefab, 20, 10);
             decal.transform.SetPositionAndRotation(hits[i].point, Quaternion.Euler(hits[i].normal));
             decal.SetActive(true);

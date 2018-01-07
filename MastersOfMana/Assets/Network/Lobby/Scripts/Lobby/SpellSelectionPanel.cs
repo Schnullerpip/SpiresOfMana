5dfd20246e2ba9f29a68baf52be8517eaca31115
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-07 14:29:12 +0100

    removed redundant event system in level & added names to the spell buttons in hierachy

--- MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/SpellSelectionPanel.cs
+++ MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/SpellSelectionPanel.cs
@@ -57,6 +57,7 @@ public class SpellSelectionPanel : MonoBehaviour {
         {
             A_Spell spell = spells[i];
             Button spellButton = GameObject.Instantiate(spellButtonPrefab);
+            spellButton.name += " " + spell.name;
             Image image = spellButton.transform.GetChild(0).GetComponent<Image>();
             if (image)
             {

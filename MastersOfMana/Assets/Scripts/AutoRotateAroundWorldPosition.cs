f01c1464b95a769a429f9a27b1e85369a838a4ba
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-07 14:51:29 +0100

    lobby cinematic camera rotates now

---
+++ MastersOfMana/Assets/Scripts/AutoRotateAroundWorldPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotateAroundWorldPosition : MonoBehaviour {
+
+    public Vector3 pivot;
+    public Vector3 axis = Vector3.up;
+    public float speed;
+
+	void Update () 
+    {
+		transform.RotateAround(pivot, axis, speed * Time.deltaTime);
+	}
+
+    private void OnValidate()
+    {
+        axis = Vector3.Normalize(axis);
+    }
+}

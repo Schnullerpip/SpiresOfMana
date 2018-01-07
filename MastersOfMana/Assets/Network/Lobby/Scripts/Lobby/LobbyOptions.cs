1d0506820349ff8e44c4ea64c5c0d3d763ca4c11
Arne <arne.knobloch@gmx.de>
2018-01-07 10:46:53 +0100

    Feature: Audio Settings and Resolution Settings

--- MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/LobbyOptions.cs
+++ MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/LobbyOptions.cs
@@ -7,19 +7,62 @@ namespace Prototype.NetworkLobby
 {
     public class LobbyOptions : MonoBehaviour
     {
-        public Dropdown controlScheme;
+        public Dropdown resolutionDropdown;
+        public Toggle fullscreenToggle;
+        public Slider volumeSlider;
+        public Toggle mutedToggle;
+        private Dictionary<int, Resolution> mResolutionDropdownMatch = new Dictionary<int, Resolution>();
+        private Dictionary<Resolution, int> mReverseResolutionDropdownMatch = new Dictionary<Resolution,int>();
+        private bool isInitialized = false;
+
+        public void Init()
+        {
+             List<string> screenResolutions = new List<string>();
+            for (int i = 0; i < Screen.resolutions.Length; i++)
+            {
+                screenResolutions.Add(Screen.resolutions[i].ToString());
+                mResolutionDropdownMatch.Add(i, Screen.resolutions[i]);
+                mReverseResolutionDropdownMatch.Add(Screen.resolutions[i], i);
+            }
+            resolutionDropdown.AddOptions(screenResolutions);
+        }
 
         public void OnEnable()
         {
-            LobbyManager.s_Singleton.SetCancelDelegate(LobbyManager.s_Singleton.SimpleBackClbk);
-            controlScheme.value = PlayerPrefs.GetInt("ControlScheme");
+            if(!isInitialized)
+            {
+                isInitialized = true;
+                Init();
+            }
+            resolutionDropdown.value = mReverseResolutionDropdownMatch[Screen.currentResolution];
+            fullscreenToggle.isOn = Screen.fullScreen;
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1);
+            mutedToggle.isOn = PlayerPrefsExtended.GetBool("muted", false);
         }
 
         public void OnSave()
         {
-            PlayerPrefs.SetInt("ControlScheme", controlScheme.value);
+            Resolution resolution = mResolutionDropdownMatch[resolutionDropdown.value];
+            Screen.SetResolution(resolution.width, resolution.height, fullscreenToggle.isOn);
+            PlayerPrefsExtended.SetResolution("resolution", resolution);
+            PlayerPrefsExtended.SetBool("fullscreen", fullscreenToggle.isOn);
+            PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+            PlayerPrefsExtended.SetBool("muted", mutedToggle.isOn);
             PlayerPrefs.Save();
-            LobbyManager.s_Singleton.Cancel();
+            LobbyManager.s_Singleton.SimpleBackClbk();
+        }
+
+        public void OnVolumeSliderChanged()
+        {
+            if (!mutedToggle.isOn)
+            {
+                AudioListener.volume = volumeSlider.value;
+            }
+        }
+
+        public void OnAudioMutedChanged()
+        {
+            AudioListener.volume = mutedToggle.isOn ? 0 : volumeSlider.value;
         }
     }
 }

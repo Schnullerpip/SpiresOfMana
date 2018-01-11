14bcf459a1da8a61aee085fc2acec32f43251e86
Arne Knobloch <arne.knobloch@gmx.de>
2018-01-11 12:02:03 +0100

    Bug: SpellHud not updated on client

--- MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/SpellSelectionPanel.cs
+++ MastersOfMana/Assets/Network/Lobby/Scripts/Lobby/SpellSelectionPanel.cs
@@ -233,6 +233,7 @@ public class SpellSelectionPanel : MonoBehaviour {
         PlayerPrefs.SetInt("SpellSlot3", mPlayerSpellList[3].spellID);
         PlayerPrefs.Save();
         mPlayerSpells.CmdUpdateSpells(mPlayerSpellList[0].spellID, mPlayerSpellList[1].spellID, mPlayerSpellList[2].spellID, mPlayerSpellList[3].spellID);
+        mPlayerSpells.UpdateSpells(mPlayerSpellList);
         mHUD.GetSpellHUD().UpdateSpellIcons();
     }
 

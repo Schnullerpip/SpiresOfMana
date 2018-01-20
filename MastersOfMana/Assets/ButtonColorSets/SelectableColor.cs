4cd9b59f8eaef78a306bd9e9e6e094850fd36539
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-20 11:59:19 +0100

    added asset creation functionality to selectionColorSets & changed text input highlight color

--- MastersOfMana/Assets/ButtonColorSets/SelectableColor.cs
+++ MastersOfMana/Assets/ButtonColorSets/SelectableColor.cs
@@ -7,8 +7,32 @@ public class SelectableColor : MonoBehaviour
 {
     public ButtonColorSet colorSet;
 
+    #if UNITY_EDITOR
+    [ContextMenu("Create ColorSet")]
+    private void CreateColorSet()
+    {
+        Selectable s = GetComponent<Selectable>();
+        ButtonColorSet asset = ScriptableObject.CreateInstance<ButtonColorSet>();
+
+        UnityEditor.AssetDatabase.CreateAsset(asset, "Assets/"+s.name+"ColorSet.asset");
+
+        asset.colors = s.colors;
+
+        UnityEditor.AssetDatabase.SaveAssets();
+        UnityEditor.AssetDatabase.Refresh();
+        UnityEditor.EditorUtility.FocusProjectWindow();
+        UnityEditor.Selection.activeObject = asset;
+
+        colorSet = asset;
+    }
+    #endif
+
     private void OnValidate()
     {
-        GetComponent<Selectable>().colors = colorSet.colors;
+        Selectable s = GetComponent<Selectable>();
+        if(s && colorSet && s.colors != colorSet.colors)
+        {
+            s.colors = colorSet.colors;
+        }
     }
 }

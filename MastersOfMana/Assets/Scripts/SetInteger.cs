00cd0e4190e510adcdb0ccfb436a9fb7ef0b5bf1
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-20 20:42:59 +0100

    restructured player animator -> prepared to include more cast animations

--- MastersOfMana/Assets/Scripts/SetInteger.cs
+++ MastersOfMana/Assets/Scripts/SetInteger.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetInteger : StateMachineBehaviour {
-
+public class SetInteger : StateMachineBehaviour 
+{
     public string parameterName;
     public int value;
+
     private int? mParameterHash;
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
     {
         if(!mParameterHash.HasValue)
         {

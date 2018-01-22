c0be49ca341a1825417b4c5ec9d0d3a532b7b351
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-22 23:12:01 +0100

    fixed triggered castfail sfx and previews in spellselection lobby

--- MastersOfMana/Assets/Scripts/States/InputStateSystem.cs
+++ MastersOfMana/Assets/Scripts/States/InputStateSystem.cs
@@ -18,7 +18,7 @@ public class InputStateSystem
 	/// if a state change occures the 'new' state can be adressed through the player's
 	/// dictionary with the respective InputStateID as the key
 	/// </summary>
-    public enum InputStateID {Normal, Hurt, Idle, Paralyzed, Dead};
+    public enum InputStateID {Normal, Hurt, Idle, Paralyzed, Dead, Lobby};
 
 
 
@@ -32,7 +32,8 @@ public class InputStateSystem
             new InputStateHurt(player),
             new InputStateIdle(player),
             new InputStateParalyzed(player),
-            new InputStateDead(player)
+            new InputStateDead(player),
+            new InputStateLobby(player)
         };
 
         current = states[0];

8818bb585a14031ad63c5a65f18c73decd03d564
Lukas Steinmetz <dr.spoik@gmail.com>
2018-01-28 13:52:15 +0100

    added button sounds, actual sound effect still subject to change

---
+++ MastersOfMana/Assets/SelectableSFXSets/SelectableSFXSet.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Selectable SFX Set", menuName = "UI FX/Selectable SFX Set", order = 1)]
+public class SelectableSFXSet : ScriptableObject
+{
+    public AudioClip enterSFX, exitSFX, clickSFX;
+}
